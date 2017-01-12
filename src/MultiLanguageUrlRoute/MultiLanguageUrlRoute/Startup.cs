2354050db89fc049220465cbe6a3259d51596104
GIT4U\jamis.liao <jamisliao@git4u.com>
2017-01-12 09:58:24 +0800

    Create Asp.net mvc and WebAPI project

---
+++ src/MultiLanguageUrlRoute/MultiLanguageUrlRoute/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MultiLanguageUrlRoute.Startup))]
+namespace MultiLanguageUrlRoute
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
